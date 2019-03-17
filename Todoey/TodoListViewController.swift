208d38016edaee33b2af1afd86efc932e94e47f4
habolanos <habolanos@localhost>
2019-03-16 19:44:02 -0500

    Implementación de persistencia: using userDefaults method

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -13,9 +13,14 @@ class TodoListViewController: UITableViewController {
     //MARK - Var Definition
     var itemArray = ["find mike", "buy eggos", "Destroy Demogorgon"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.value(forKey: "TodoItemArray") as? [String]{
+            itemArray = items
+        }
     }
 
     //MARK - TableView Datasource Methods
@@ -54,6 +59,9 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoItemArray")
+            
             self.tableView.reloadData()
         }
         
